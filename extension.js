62f6c1914e3ab9696b2388b1087bfd4b91687784
Muhammad Muneer <MdMuneer310@gmail.com>
2024-02-26 23:57:32 +0530

    cleanup

--- extension.js
+++ extension.js
@@ -1,23 +1,21 @@
 chrome.webNavigation.onCompleted.addListener(
-    function (details) {
-      // Check if the loaded page is from LinkedIn
-      if (isLinkedIn(details.url)) {
-        // Execute content script to block ads
-        chrome.scripting.executeScript({
-          target: { tabId: details.tabId },
-          files: ["linkedinAdBlocker.js"]
-        });
-      }
-    },
-    {
-      // Specify the filter to listen only for main frames
-      url: [{ urlMatches: ".*linkedin.com.*" }],
-      types: ["main_frame"],
+  function (details) {
+    // Check if the loaded page is from LinkedIn
+    if (isLinkedIn(details.url)) {
+      // Execute content script to block ads
+      chrome.scripting.executeScript({
+        target: { tabId: details.tabId },
+        files: ["linkedinAdBlocker.js"],
+      });
     }
-  );
-  
-  // Function to check if a given URL is from LinkedIn
-  function isLinkedIn(url) {
-    return url.includes("linkedin.com");
+  },
+  {
+    // Specify the filter to listen only for main frames
+    url: [{ urlMatches: ".*linkedin.com.*" }],
+    types: ["main_frame"],
   }
-  
+);
+
+function isLinkedIn(url) {
+  return url.includes("linkedin.com");
+}
